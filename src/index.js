573a7d24020ffb95fb8c3527d10fb1e6bdea4c45
JenyaOst <gjhit922@gmail.com>
2023-11-17 18:58:47 +0200

    Change3

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import 'slim-select/dist/slimselect.css';
 
+const selectPlaceholder = `<option class="js-selectOption js-placeholder-select" value="choose">Select the cat</option>`;
 const ref = {
     selector: document.querySelector('.breed-select'),
     divCatInfo: document.querySelector('.cat-info'),
@@ -12,12 +13,10 @@ const ref = {
 
 const { selector, divCatInfo, loader, error } = ref;
 
-loader.classList.replace('loader', 'is-hidden');
-error.classList.add('is-hidden');
-divCatInfo.classList.add('is-hidden');
-
-const selectPlaceholder = `<option class="js-selectOption js-placeholder-select" value="choose">Select the cat</option>`;
 selector.insertAdjacentHTML("afterbegin", selectPlaceholder);
+
+selector.hidden = true;
+loader.hidden = false;
 
 function markupSelect(arr) {
     return arr.map(({name, id}) => {
@@ -29,33 +28,37 @@ function markupSelect(arr) {
 
 fetchBreeds()
 .then(data => {
+ 
     selector.insertAdjacentHTML("beforeend", markupSelect(data));
+    selector.hidden = false;
+    loader.hidden = true;
+
     })
 .catch(onFetchError);
 
 selector.addEventListener('change', onSelectBreed);
 
 function onSelectBreed(event) {
-    loader.classList.replace('is-hidden', 'loader');
-    selector.classList.add('is-hidden');
-    divCatInfo.classList.add('is-hidden');
-
+    error.hidden = true;
+    loader.hidden = false;
+    selector.hidden = false;
+    divCatInfo.classList.add("cat-card");
     const breedId = event.currentTarget.value;
     fetchCatByBreed(breedId)
     .then(data => {
-        loader.classList.replace('loader', 'is-hidden');
-        selector.classList.remove('is-hidden');
         const { url, breeds } = data[0];
         divCatInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`
-        divCatInfo.classList.remove('is-hidden');
+        loader.hidden = true;
+selector.hidden = false;
+divCatInfo.classList.remove("cat-card");
     })
     .catch(onFetchError);
 };
 
 function onFetchError(error) {
-    selector.classList.remove('is-hidden');
-    loader.classList.replace('loader', 'is-hidden');
-
+    selector.hidden = false;
+    loader.hidden = true;
+    divCatInfo.classList.add("cat-card");
     Notify.failure('Oops! Something went wrong! Try reloading the page or select another cat breed!', {
         position: 'center-center',
         timeout: 5000,
